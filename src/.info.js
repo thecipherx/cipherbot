c06c11cb9f668cb11c43e8b37dcdcfdc79b903f3
CipherX <49910170+TheCipherX@users.noreply.github.com>
2020-05-30 22:32:38 +0530

    May 30 2k20

---
+++ src/.info.js
@@ -0,0 +1,71 @@
+/**
+ * its V3 version Of Cipher bot
+ * Hello Machanzz.. I made this tool. I'm Mohammed Khalid Sha
+
+ *Actually I Cracked Many Tools and Mod it in a Single Tool that's Cipherbot
+ * And this is collection of several developments that have been carried out so far
+ *if you recode this plz give me credits
+ *
+ *
+ *    Machanmare Insta Follow Cheyyan Marakkalle
+ *       insta id @the_khalid_sha , @solo_comrade_ , @_dark_prince._ 
+ *
+ *
+ */
+'use strict'
+const delay = require('delay');
+const chalk = require('chalk');
+const inquirer = require('inquirer');
+const main = async () => {
+  var spinner;
+  }
+
+console.log(chalk `{bold.green
+   ____  _  ___   _  _   ____  ____      
+   |     |  |__]  |__|   |___  |__/      
+   |___  |  |     |  |   |___  |  \
+                             ___  ____ ___ 
+                             |__] |  |  |  
+ {bold.red "©2020 Security Exploiters"} |__] |__|  |  
+ {bold.red IMPORTANT NOTE : 
+                THE DELAY YOU HAVE TO SET IN TOOL IS IN MILLISECONDS}
+ }`);
+console.log(chalk `{bold.yellow {bold.cyan  =====================================================}
+ |      CIPHER INSTAGRAM BOT v3.2                    |
+ |  {bold.green "KHALID SHA"} [The CipherX]- The Author           |
+ |        @the_khalid_sha [INSTAGRAM]                |
+ | git clone https://github.com/TheCipherX/cipherbot |}`);
+console.log(chalk `{bold.cyan  =====================================================
+} `);      
+console.log(chalk `{bold.yellow
+ CONTACT ME ON INSTAGRAM : @the_khalid_sha
+ CONTACT ME ON WHATSAPP  : +15817003192
+ CONTACT ME ON EMAIL     : shadohackers@gmail.com
+ OUR GITHUB WEBSITE      : https://github.com/TheCipherX/
+} `);
+console.log(chalk `{bold.green 
+CIPHER BOT THE #1 INSTA AUTOMATION TOOL
+{bold.cyan =+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=}
+
+  >> Note : it's not a tool which gives you 
+       free followers and likes by doing nothing....
+   {bold.cyan ========================================================}
+  >> it's a automation tool which helps you to give 
+       Likes & Comments to other posts using your account...
+   {bold.cyan ========================================================}
+  >> it makes your job done easily, that you don't want to 
+       waste time by liking and commenting clebrities and 
+       hashtags posts for getting insta support....
+   {bold.cyan ========================================================}    
+  >> this tool itself act as a robot and like post 
+       based on hashtags and peoples, so that the percentage 
+       of chance to get geniune support on instagram 
+       will increase....
+   {bold.cyan ========================================================}    
+   {bold.red IMPORTANT NOTE : 
+             ***THE DELAY YOU HAVE TO SET IN TOOL IS IN MILLISECONDS
+                SET DELAY ONLY IN MILLISECONDS}
+}  `);
+
+
+main()
